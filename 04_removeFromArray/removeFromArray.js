14dcff588cae45385bfc464c7f3c6961a4328546
Antony <45654017+Antonymous1337@users.noreply.github.com>
2023-03-31 21:19:51 -0700

    Finished Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    const ARGS = arguments;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < ARGS.length; j++) {
+            if (arr[i] === ARGS[j]) {
+                arr.splice(i, 1);
+                j = 0;
+            }
+        }
+    }
 
+    return arr;
 };
 
 // Do not edit below this line
