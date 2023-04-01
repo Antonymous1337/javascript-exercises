5e5367750c6eacfe4da8efc1f365ed916e788333
Antony <45654017+Antonymous1337@users.noreply.github.com>
2023-03-31 21:27:28 -0700

    Finished Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(beginning, end) {
+    if (typeof beginning !== "number" || typeof end !== "number") return "ERROR";
+    if (beginning < 0 || end < 0) return "ERROR";
 
+    if (beginning > end) {
+        let temp = beginning;
+        beginning = end;
+        end = temp;
+    }
+
+    let res = 0;
+    for (let i = beginning; i <= end; i++) res += i;
+    return res;
 };
 
 // Do not edit below this line
